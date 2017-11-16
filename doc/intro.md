06f77aea35f6bc49656e2362801df3444c601b45
didier amyot <didier.amyot@gmail.com>
2017-11-15 22:11:02 -0500

    started to play around with plotly

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to graphly
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject graphly "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
+                 [clj-http "3.7.0"]]
+  :main ^:skip-aot graphly.core
+  :plugins [[lein-jupyter "0.1.10"]]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
